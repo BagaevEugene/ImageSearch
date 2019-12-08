b7b36e60177fc3165d52871fb0ba02c578cf73cb
BagaevEugene <36105583+BagaevEugene@users.noreply.github.com>
2019-12-08 15:46:01 +0700

    Initial Commit

---
+++ ImageSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageSearch
+//
+//  Created by Eugene Bagaev on 08.12.2019.
+//  Copyright © 2019 Eugene Bagaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
